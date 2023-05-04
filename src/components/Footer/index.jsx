d933990fc6e2c893b48a502d97f43937e45bdec7
Denilson Rocha <denilsonrocha_@outlook.com>
2023-05-04 18:26:36 -0300

    remove tag nav e efeitos de hover do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,34 +2,32 @@ import "./style.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <nav className="nav">
-        <ul className="nav-ul">
-          <li className="nav-ul--li">
-            <span>F1</span>
-            <p> Help</p>
-          </li>
-          <li className="nav-ul--li">
-            <span>F2</span>
-            <p> Save</p>
-          </li>
-          <li className="nav-ul--li">
-            <span>F3</span>
-            <p> Open</p>
-          </li>
-          <li className="nav-ul--li">
-            <span>Alt-F9</span>
-            <p> Compile</p>
-          </li>
-          <li className="nav-ul--li">
-            <span>F9</span>
-            <p> Make</p>
-          </li>
-          <li className="nav-ul--li">
-            <span>F10</span>
-            <p> Menu</p>
-          </li>
-        </ul>
-      </nav>
+      <ul className="nav-ul">
+        <li className="nav-ul--li">
+          <span>F1</span>
+          <p> Help</p>
+        </li>
+        <li className="nav-ul--li">
+          <span>F2</span>
+          <p> Save</p>
+        </li>
+        <li className="nav-ul--li">
+          <span>F3</span>
+          <p> Open</p>
+        </li>
+        <li className="nav-ul--li">
+          <span>Alt-F9</span>
+          <p> Compile</p>
+        </li>
+        <li className="nav-ul--li">
+          <span>F9</span>
+          <p> Make</p>
+        </li>
+        <li className="nav-ul--li">
+          <span>F10</span>
+          <p> Menu</p>
+        </li>
+      </ul>
     </footer>
   );
 };
