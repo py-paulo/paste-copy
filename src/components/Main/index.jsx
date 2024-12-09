bcc2330a78ecea573e0cb6b64eff2790b41053e8
Paulo Roberto <paulo.rb.beserra@gmail.com>
2024-12-09 03:44:12 -0300

    feat: basic copy and paste by room;

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,5 +1,49 @@
+import React, { useEffect, useRef, useState } from "react";
+import throttle from "lodash.throttle";
+import useWebSocket from "react-use-websocket";
 import "./style.css";
-const Main = () => {
+
+const Main = ({ username, note }) => {
+  const WS_URL = `ws://127.0.0.1:8000/${note}`;
+
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
+    queryParams: { username },
+    share: true,
+  });
+
+  const THROTTLE = 50;
+  const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
+
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    if (lastJsonMessage) {
+      let latestMessage = null;
+
+      Object.keys(lastJsonMessage).forEach((uuid) => {
+        const data = lastJsonMessage[uuid];
+        const datatime = new Date(data.state.datatime);
+
+        if (!latestMessage || datatime > new Date(latestMessage.state.datatime)) {
+          latestMessage = data;
+        }
+      });
+
+      if (latestMessage) {
+        textareaRef.current.value = latestMessage.state.text;
+      }
+    }
+  }, [lastJsonMessage]);
+
+  const handleTextareaChange = (e) => {
+    const text = e.target.value;
+    sendJsonMessageThrottled.current({
+      username,
+      text,
+      datatime: new Date().toISOString(),
+    });
+  };
+
   return (
     <main className="main">
       <div className="painel">
@@ -7,24 +51,24 @@ const Main = () => {
           <div className="painel-header--border---left">
             <span className="icon-border">
               <span>[</span>
-
               <span className="icon-border-inside"></span>
-
               <span>]</span>
             </span>
           </div>
-          <div className="painel-header---title">NONAME00.CPP</div>
+          <div className="painel-header---title">{note}.CPP</div>
           <div className="painel-header--border---right" />
         </div>
         <div className="painel-main">
-          {" "}
           <textarea
             name=""
-            id=""
+            id="editor"
             cols="30"
             rows="10"
             className="painel-main--input"
-          ></textarea>
+            onKeyUpCapture={handleTextareaChange}
+            ref={textareaRef}
+          >
+          </textarea>
         </div>
         <div className="painel-footer">
           <div className="painel-footer--border" />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter, Routes, Route  } from "react-router";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/:hash" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+  /*
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  */
 )
